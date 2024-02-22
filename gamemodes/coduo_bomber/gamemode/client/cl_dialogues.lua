5210df44af0babc6c89a324d42c366f4edf927ba
kartjom <jakubrutkowski3012@gmail.com>
2024-02-22 20:28:58 +0100

    Fixed dialogues cutting out

--- gamemodes/coduo_bomber/gamemode/client/cl_dialogues.lua
+++ gamemodes/coduo_bomber/gamemode/client/cl_dialogues.lua
@@ -1,5 +1,5 @@
 function DialoguePlay(sndPath)
-    EmitSound( Sound(sndPath), LocalPlayer():GetPos(), -2, CHAN_VOICE, 1, 75, SND_SHOULDPAUSE, 100, 0 )
+	surface.PlaySound( Sound(sndPath) )
 end
 
 net.Receive("NET_DIALOGUE", function()
